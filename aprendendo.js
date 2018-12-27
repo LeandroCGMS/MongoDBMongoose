674fdf296c1f703efcea2c6be93c2f49bb8aac9b
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-27 17:08:04 -0300

    Curso de Node.js - Trabalhando com o Mongoose #30

--- aprendendo.js
+++ aprendendo.js
@@ -9,3 +9,44 @@ mongoose.connect("mongodb://localhost/aprendendo", {
 }).catch((erro) => {
     console.log("Houve um erro ao conectar. Erro: " + erro)
 })
+
+// Model - Usuários
+// Definindo o Model
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+
+// Collection
+mongoose.model('usuarios', UsuarioSchema)
+
+const Victor = mongoose.model('usuarios')
+
+new Victor({
+    nome: "Jhon",
+    sobrenome: "Joe",
+    email: "jhon@joe.com",
+    idade: 34,
+    pais: "EUA"
+}).save().then(() => {
+    console.log("Usuário criado com sucesso.")
+}).catch((erro) => {
+    console.log("Houve um erro ao registrar usuário. Erro: " + erro)
+})
