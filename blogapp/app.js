b0e20c4456dce0f67ff89497818fcef0646076f1
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-28 10:34:33 -0300

    Curso de Node.js - Grupo de rotas no Express.js #32

--- blogapp/app.js
+++ blogapp/app.js
@@ -3,6 +3,7 @@
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
     const app = express()
+    const admin = require('./routes/admin')
     //const mongoose = require('mongoose')
 
 // Configurações
@@ -17,7 +18,15 @@
     //
 
 // Rotas
+    app.get('/', (req, res) => {
+        res.send("Rota principal")
+    })
 
+    app.get('/posts', (req, res) => {
+        res.send("Lista de Posts")
+    })
+
+    app.use('/admin', admin)
 
 // Outros
 const PORT = 8081;
