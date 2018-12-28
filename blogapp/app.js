f56af5bf4b12986948a0b17f879476079236ce00
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-28 11:09:30 -0300

    Curso de Node.js - Arquivos estáticos #33

--- blogapp/app.js
+++ blogapp/app.js
@@ -4,18 +4,20 @@
     const bodyParser = require('body-parser')
     const app = express()
     const admin = require('./routes/admin')
+    const path = require('path')
     //const mongoose = require('mongoose')
 
 // Configurações
     // Body Parser
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
     // Handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
     // Mongoose
         // Em breve
-    //
+    // Public
+        app.use(express.static(path.join(__dirname,"public")))
 
 // Rotas
     app.get('/', (req, res) => {
