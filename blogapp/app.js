5bb66d2547f5100c143d1af57135398d55289c05
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-29 10:53:51 -0300

    Curso de Node.js - Definindo o model de categorias #35

--- blogapp/app.js
+++ blogapp/app.js
@@ -5,7 +5,7 @@
     const app = express()
     const admin = require('./routes/admin')
     const path = require('path')
-    //const mongoose = require('mongoose')
+    const mongoose = require('mongoose')
 
 // Configurações
     // Body Parser
@@ -15,7 +15,12 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
-        // Em breve
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/blogapp").then(() => {
+            console.log("Conectado ao Mongo.")
+        }).catch((erro) => {
+            console.log("Ocorreu um erro ao conectar-se no Mongo. Erro: " + erro)
+        })
     // Public
         app.use(express.static(path.join(__dirname,"public")))
 
