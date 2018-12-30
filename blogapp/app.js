5ab675a4bbbb36b3c526b357e3e34a1aae1aab66
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-30 11:12:34 -0300

    Curso de Node.js - Como configurar sessões #39

--- blogapp/app.js
+++ blogapp/app.js
@@ -6,8 +6,22 @@
     const admin = require('./routes/admin')
     const path = require('path')
     const mongoose = require('mongoose')
-
+    const session = require('express-session')
+    const flash = require('connect-flash')
 // Configurações
+    // Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
     // Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -24,10 +38,10 @@
     // Public
         app.use(express.static(path.join(__dirname,"public")))
 
-        app.use((req, res, next) => {
+        /*app.use((req, res, next) => {
             console.log("OI, EU SOU UM MIDDLEWARE.")
             next()
-        })
+        })*/
 
 // Rotas
     app.get('/', (req, res) => {
