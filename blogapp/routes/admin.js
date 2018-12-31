a0649740087c3d53e871dbdb5e83aa3c188d6c77
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-31 10:32:40 -0300

    Curso de Node.js - Editando categoria #42

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -60,6 +60,32 @@ router.post('/categorias/nova', (req, res) => {
     }
 })
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id: req.params.id}).then((categoria) => {
+        res.render('admin/editcategorias', {categoria: categoria})
+    }).catch((erro) => {
+        req.flash("error_msg", "Esta categoria não existe.")
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post('/categorias/edit', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+        categoria.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucesso")
+            res.redirect('/admin/categorias')
+        }). catch((erro) => {
+            req.flash("error_msg", "Houve erro ao salvar categoria da edição.")
+            res.redirect('/admin/categorias')
+        })
+    }).catch((erro) => {
+        req.flash("error_msg", "Houve um erro ao editar a categoria.")
+        res.redirect('/admin/categorias')
+    })
+})
+
 router.get('/teste', (req, res) => {
     res.send("Isso é um teste")
 })
