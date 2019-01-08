c80b876e60eb3e1da10ae28ce7b210149782cd04
LeandroCGMS <leandrocgms2015@gmail.com>
2019-01-08 18:45:59 -0300

    Curso de Node.js - Cadastrando usuários no banco de dados #56

--- blogapp/routes/usuario.js
+++ blogapp/routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Usuario')
 const Usuario = mongoose.model("usuarios")
+const bcrypt = require('bcryptjs')
 
 router.get('/registro', (req, res) => {
     res.render('usuarios/registro')
@@ -34,7 +35,41 @@ router.post('/registro', (req, res) => {
     if(erros.length > 0){
         res.render('usuarios/registro', {erros: erros})
     } else {
-        // Na próxima aula
+        Usuario.findOne({email: req.body.email}).then((usuario) => {
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este e-mail.")
+                res.redirect('/usuarios/registro')
+            } else {
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro){
+                            req.flash("error_msg", "Não foi possível tornar a sua senha segura," +
+                            " por isso foi abortado o procedimento. Erro: " + erro)
+                            res.redirect('/')
+                        } else {
+                            novoUsuario.senha = hash
+                            novoUsuario.save().then(() => {
+                                req.flash("success_msg", "Usuário criado com sucesso.")
+                                res.redirect('/')
+                            }).catch((erro) => {
+                                req.flash("error_msg", "Houve um erro ao salvar usuário. Erro: " + erro)
+                                res.redirect('/usuarios/registro')
+                            })
+                        }
+                    })
+                })
+            }
+        }).catch((erro) => {
+            req.flash("error_msg", "Houve um erro ao tentar encontrar um usuário com o e-mail" +
+            " solicitado. Erro: " + erro)
+            res.redirect('/')
+        })
     }
 })
 
